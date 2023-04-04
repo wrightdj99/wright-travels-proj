f76b6d9c4ad51b2a47b963160dfd0a93c4b19f05
wrightdj99 <wrightdj99@gmail.com>
2023-04-04 18:28:04 -0500

    test

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,6 +5,7 @@ function Homepage() {
     <div className="App">
       <h1>Wright Travel Agency</h1>
       <h3>Wright your own ticket to a fabulous trip!</h3>
+      <p>We're going to Azure!</p>
     </div>
     );
 }
