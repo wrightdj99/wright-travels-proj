a7529d83902fae91500c6636f0cc220ac84fab10
wrightdj99 <wrightdj99@gmail.com>
2023-04-04 17:54:26 -0500

    Commit made from VS Code Azure Static Web Apps

---
+++ src/components/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/HotelStyles.css';
+function Homepage() {
+    return(
+    <div className="App">
+      <h1>Wright Travel Agency</h1>
+      <h3>Wright your own ticket to a fabulous trip!</h3>
+    </div>
+    );
+}
+
+export default Homepage;
